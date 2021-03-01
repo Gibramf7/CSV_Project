0f96368e8f59e0349a9dff737e273b6b957ac4be
Gibram Favela <Gibram_Favela1@baylor.edu>
2021-03-01 15:11:30 -0600

    eighth commit

--- CSV_PROJECT.py
+++ CSV_PROJECT.py
@@ -2,52 +2,53 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-filename = 'death_valley_2018_simple.csv'
 filename = 'sitka_weather_2018_simple.csv'
+
 place_name = ''
+fig, ax = plt.subplots(2)
+for i in range (2):
 
-with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
 
-    print(header_row)
-    date_index = header_row.index('DATE')
-    high_index = header_row.index('TMAX')
-    low_index = header_row.index('TMIN')
-    name_index = header_row.index('NAME')
+        print(header_row)
+        date_index = header_row.index('DATE')
+        high_index = header_row.index('TMAX')
+        low_index = header_row.index('TMIN')
+        name_index = header_row.index('NAME')
 
 
-    dates = []
-    highs =[]
-    lows = []
-    for row in reader:
-        if not place_name:
+        dates = []
+        highs =[]
+        lows = []
+        for row in reader:
             place_name = row[name_index]
             print(place_name)
             
-        converted_date = datetime.strptime(row[date_index], '%Y-%m-%d')
-        try:
-            high = int(row[high_index])
-            low = int(row[low_index])
-        except ValueError:
-            print(f"Missing data for {converted_date}")
-        else:
-            dates.append(converted_date)
-            highs.append(high)
-            lows.append(low)
+            converted_date = datetime.strptime(row[date_index], '%Y-%m-%d')
+            try:
+                high = int(row[high_index])
+                low = int(row[low_index])
+            except ValueError:
+                print(f"Missing data for {converted_date}")
+            else:
+                dates.append(converted_date)
+                highs.append(high)
+                lows.append(low)
+        #fig, ax = plt.subplots(2)
+        # Plot the high and low temperatures
+        ax[i].plot(dates, highs, c='red', alpha=0.5)
+        ax[i].plot(dates, lows, c='blue', alpha=0.5)
+        ax[i].fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+        ax[i].set_title(place_name, fontsize=12)
 
-# Plot the high and low temperatures.
-fig, ax = plt.subplots(2)
-ax[0].plot(dates, highs, c='red', alpha=0.5)
-ax[0].plot(dates, lows, c='blue', alpha=0.5)
-plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-ax[1].plot(dates, highs, c='red', alpha=0.5)
-ax[1].plot(dates, lows, c='blue', alpha=0.5)
-plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+        filename = 'death_valley_2018_simple.csv'
+        i += 1
 
 # Format plot.
-suptitle = f"Temperature comparison between {place_name} and {place_name}"
+suptitle = f"Temperature comparison between SITKA AIRPORT, AK US and {place_name}"
 plt.suptitle(suptitle, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
